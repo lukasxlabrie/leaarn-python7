f33450460882fb09cc9fa8dcf4e27272d263c429
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-04 18:31:59 -0400

    rmeoved space

--- ex7.py
+++ ex7.py
@@ -1,5 +1,5 @@
 print("Mary had a little lamb.")
-print("Its fleece was white  as {}.".format('snow'))
+print("Its fleece was white as {}.".format('snow'))
 print ("And everywhere that Mary went.")
 print("." * 10) #What'd that do?
 
