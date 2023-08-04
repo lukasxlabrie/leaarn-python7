becf42efe3116ceea2f5add8bb226566e89e96ef
misosucky <126988962+Misosucky@users.noreply.github.com>
2023-08-04 18:39:13 -0400

    notes

--- ex7.py
+++ ex7.py
@@ -4,16 +4,17 @@ print("Mary had a little lamb.")
 print("Its fleece was white as {}.".format('snow'))
 #option2 = 'snow'
 #print(f"Its fleece was white as {option2}")
+#this is just anoher print command
 print ("And everywhere that Mary went.")
-print("." * 10) #What'd that do?
-
+print("." * 10) #What'd that do? this tells the program to print 10 '.'
+#these are all var
 end1 = "C"
 end2 = "h"
 end3 = "e"
 end4 = "e"
 end5 = "s"
 end6 = "e"
-end7 = "B"
+end7 = "B" 
 end8 = "u"
 end9 = "r"
 end10 = "g"
